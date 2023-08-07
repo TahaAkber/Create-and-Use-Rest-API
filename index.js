76fb1a04cc1774d2c4edf52db8fb37e965b3d330
Taha <taha.akber007@gmail.com>
2023-08-07 16:04:11 +0500

    Update delete and find

--- index.js
+++ index.js
@@ -15,13 +15,44 @@ app.post("/mens", async (req, res) => {
     res.status(400).send(error);
   }
 });
-
+//get all user
 app.get("/mens", async (req, res) => {
   try {
-    const getMens = await MensRanking.find({});
+    const getMens = await MensRanking.find();
     res.send(getMens);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+//get individual
+app.get("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getMen = await MensRanking.findById({ _id: _id });
+    res.send(getMen);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//find by id and update
+app.patch("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getmember = await MensRanking.findByIdAndUpdate(_id, req.body);
+    res.send(getmember);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//find by id and delete
+app.delete("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const delmember = await MensRanking.findByIdAndDelete(_id);
+    res.send(delmember);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 app.listen(port, () => {
